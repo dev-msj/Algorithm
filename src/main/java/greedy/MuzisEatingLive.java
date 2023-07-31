f6074ab746940b7c92414b5dc8ba0150e3bfa5df
dev-msj <msj944@naver.com>
2023-08-01 01:31:13 +0900

    그리디 기출 : 무지의 먹방 라이브 풀이 실패

--- src/main/java/greedy/MuzisEatingLive.java
+++ src/main/java/greedy/MuzisEatingLive.java
@@ -1,8 +1,34 @@
 package greedy;
 
+import java.util.ArrayDeque;
+import java.util.Queue;
+
 public class MuzisEatingLive {
-    public int solution(int[] food_times, long k) {
-        int answer = 0;
-        return answer;
+    public static int solution(int[] food_times, long k) {
+        Queue<Integer> queue = new ArrayDeque<>();
+        for (int i = 0; i < k; i++) {
+            int idx;
+            if (i < food_times.length) {
+                idx = i % food_times.length;
+            } else {
+                if (queue.isEmpty()) {
+                    break;
+                }
+
+                idx = queue.poll();
+            }
+
+            food_times[idx] -= 1;
+
+            if (food_times[idx] > 0) {
+                queue.offer(idx);
+            }
+        }
+
+        if (queue.isEmpty()) {
+            return -1;
+        }
+
+        return queue.poll() + 1;
     }
 }
--- src/test/java/greedy/MuzisEatingLiveTest.java
+++ src/test/java/greedy/MuzisEatingLiveTest.java
@@ -1,14 +1,18 @@
 package greedy;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+
+import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class MuzisEatingLiveTest {
-    @ParameterizedTest
-    @CsvSource
+//    @ParameterizedTest
+//    @CsvSource(value = {"3 1 2:5:1"}, delimiter = ':')
+    @Test
     void solutionTest() {
-
+        assertEquals(1, MuzisEatingLive.solution(new int[] {3, 1, 2}, 5));
     }
 }
