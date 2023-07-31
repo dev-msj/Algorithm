208212e35ef53c7fe569ac4a3db2294ade2991fb
dev-msj <msj944@naver.com>
2023-07-31 23:50:16 +0900

    그리디 기출 : 무지의 먹방 라이브 코드 생성

---
+++ src/main/java/greedy/MuzisEatingLive.java
@@ -0,0 +1,8 @@
+package greedy;
+
+public class MuzisEatingLive {
+    public int solution(int[] food_times, long k) {
+        int answer = 0;
+        return answer;
+    }
+}
---
+++ src/test/java/greedy/MuzisEatingLiveTest.java
@@ -0,0 +1,14 @@
+package greedy;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MuzisEatingLiveTest {
+    @ParameterizedTest
+    @CsvSource
+    void solutionTest() {
+
+    }
+}
